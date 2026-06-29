00db0494e65f92d81f0ca862a06881323285716e
hamdynydhal-arch <hamdynydhal@gmail.com>
2026-06-30 00:26:22 +0300

    feat: turn app into PWA with custom mobile install popup featuring a goat icon

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-var CACHE = 'goats-v1';
-var FILES = [
+var CACHE = 'qatee3-v3';
+var CORE = [
   '/goats-project/',
-  '/goats-project/index.html'
+  '/goats-project/index.html',
+  '/goats-project/manifest.json',
+  '/goats-project/goat-arab.jpg',
+  '/goats-project/goat-damascus.jpg',
+  '/goats-project/goat-boer.jpg',
+  '/goats-project/feed-barley.jpg',
+  '/goats-project/feed-wheat-bran.jpg',
+  '/goats-project/feed-soybean.jpg',
+  '/goats-project/feed-calcium.jpg',
+  '/goats-project/feed-alfalfa.jpg',
+  '/goats-project/feed-vitamins.jpg'
 ];
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
-  );
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(CORE); }));
   self.skipWaiting();
 });
 
@@ -21,15 +29,17 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
     caches.match(e.request).then(function(cached) {
-      return cached || fetch(e.request).then(function(res) {
-        var clone = res.clone();
-        caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+      var netFetch = fetch(e.request).then(function(res) {
+        if (res && res.status === 200) {
+          var clone = res.clone();
+          caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+        }
         return res;
-      });
-    }).catch(function() {
-      return caches.match('/goats-project/index.html');
+      }).catch(function() { return cached; });
+      return cached || netFetch;
     })
   );
 });
